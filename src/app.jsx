d62e56d309d7c927173bc6e9d7425cf1ee34874b
Mason Smith <mason.smith94@outlook.com>
2018-06-13 22:18:13 -0700

    Added live store

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter.jsx';
 import configureStore from './store/configureStore.jsx';
 import 'normalize.css/normalize.css';
@@ -19,4 +20,10 @@ const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+  <Provider store={ store }>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
--- src/components/ExpenseDashboardPage.jsx
+++ src/components/ExpenseDashboardPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import ExpenseList from './ExpenseList.jsx';
+import ExpenseListFilters from './ExpenseListFilters.jsx'
 
 const ExpenseDashboardPage = () => (
   <div>
-    This is From my dashboard component
+    <ExpenseListFilters />
+    <ExpenseList />
   </div>
 )
 
