20c47b2168aee8488a7ead3dc68d34239927a181
Mason Smith <mason.smith94@outlook.com>
2018-06-19 19:52:30 -0700

    Testing Header component with Jest

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <div>
-      <NavLink to="/" activeClassName="is-active" exact={true}> Dashboard</NavLink>
-      <NavLink to="/create" activeClassName="is-active"> Create Expense </NavLink>
-      <NavLink to="/help" activeClassName="is-active"> Get Help </NavLink>
-    </div>
-  </header>
-)
+ <header>
+  <h1>Expensify</h1>
+  <div>
+   <NavLink to="/" activeClassName="is-active" exact={true}>
+    {' '}
+    Dashboard
+   </NavLink>
+   <NavLink to="/create" activeClassName="is-active">
+    {' '}
+    Create Expense{' '}
+   </NavLink>
+  </div>
+ </header>
+);
 
 export default Header;
