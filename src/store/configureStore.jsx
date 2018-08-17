566bb89a39348f1868db6c1d2ec594753655773f
Rauch Industries <rauchindustries@Rauchs-MacBook-Pro.local>
2018-08-16 23:12:19 -0700

    Testing EditExpensePage

--- src/store/configureStore.jsx
+++ src/store/configureStore.jsx
@@ -1,15 +1,18 @@
-import { createStore, combineReducers } from 'redux';
-import expensesReducer from '../reducers/expenses.jsx';
-import filtersReducer from '../reducers/filters.jsx';
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import expensesReducer from "../reducers/expenses.jsx";
+import filtersReducer from "../reducers/filters.jsx";
+import thunk from "redux-thunk";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => {
   const store = createStore(
     combineReducers({
-     expenses: expensesReducer,
-     filters: filtersReducer
+      expenses: expensesReducer,
+      filters: filtersReducer
     }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(applyMiddleware(thunk))
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
   return store;
 };
-
--- webpack.config.js
+++ webpack.config.js
@@ -2,48 +2,46 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = env => {
- const isProduction = env === 'production';
- const CSSExtract = new ExtractTextPlugin('styles.css');
- return {
-  entry: './src/app.jsx',
-  output: {
-   path: path.join(__dirname, 'public', 'dist'),
-   filename: 'bundle.js'
-  },
-  module: {
-   rules: [
-    {
-     loader: 'babel-loader',
-     test: /\.jsx$/,
-     exclude: /node_modules/
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
+  return {
+    entry: './src/app.jsx',
+    output: {
+      path: path.join(__dirname, 'public', 'dist'),
+      filename: 'bundle.js'
     },
-    {
-     test: /\.s?css$/,
-     use: CSSExtract.extract({
-      use: [
-       {
-        loader: 'css-loader',
-        options: {
-         sourceMap: true
+    module: {
+      rules: [{
+          loader: 'babel-loader',
+          test: /\.jsx$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [{
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
         }
-       },
-       {
-        loader: 'sass-loader',
-        options: {
-         sourceMap: true
-        }
-       }
       ]
-     })
+    },
+    plugins: [CSSExtract],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
-   ]
-  },
-  plugins: [CSSExtract],
-  devtool: isProduction ? 'source-map' : 'inline-source-map',
-  devServer: {
-   contentBase: path.join(__dirname, 'public'),
-   historyApiFallback: true,
-   publicPath: '/dist/'
-  }
- };
+  };
 };
