7346416ed86ebdaf7258cd1fb2fc84117d907632
Rauch Industries <rauchindustries@Rauchs-MacBook-Pro.local>
2018-08-22 14:45:29 -0700

    Fixed vulnerable dependency and started testing async redux actions

--- src/tests/actions/expenses.test.jsx
+++ src/tests/actions/expenses.test.jsx
@@ -1,5 +1,14 @@
-import { addExpense, editExpense, removeExpense } from "../../actions/expenses";
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import {
+  startAddExpense,
+  addExpense,
+  editExpense,
+  removeExpense
+} from "../../actions/expenses";
 import expenses from "../fixtures/expenses";
+
+const createMockStore = configureMockStore([thunk]);
 
 test("should setup remove expense action object", () => {
   const action = removeExpense({ id: "123abc" });
@@ -27,6 +36,22 @@ test("should setup add expense action object with provided values", () => {
     expense: expenses[2]
   });
 });
+
+test("should add expense to database and store", done => {
+  const store = createMockStore({});
+  const expenseData = {
+    description: "Mouse",
+    note: "My slick new wireless mouse!",
+    amount: 39900,
+    createdAt: 68723484
+  };
+  store.dispatch(startAddExpense(expenseData)).then(() => {
+    expect(1).toBe(1);
+    done();
+  });
+});
+
+test("should add expense with defaults to database and store", () => {});
 
 // test('should setup add expense action object with default values', () => {
 //  const expenseData = {
