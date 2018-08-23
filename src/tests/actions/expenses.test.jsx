59d6ae054bd7663247982a94890bdb76cf9f30ec
mason-smith <mason.smith94@outlook.com>
2018-08-23 16:38:19 -0700

    Completed testing async redux actions

--- src/tests/actions/expenses.test.jsx
+++ src/tests/actions/expenses.test.jsx
@@ -7,6 +7,7 @@ import {
   removeExpense
 } from "../../actions/expenses";
 import expenses from "../fixtures/expenses";
+import database from "../../firebase/firebase";
 
 const createMockStore = configureMockStore([thunk]);
 
@@ -45,13 +46,51 @@ test("should add expense to database and store", done => {
     amount: 39900,
     createdAt: 68723484
   };
-  store.dispatch(startAddExpense(expenseData)).then(() => {
-    expect(1).toBe(2);
-    done();
-  });
+  store
+    .dispatch(startAddExpense(expenseData))
+    .then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual({
+        type: "ADD_EXPENSE",
+        expense: {
+          id: expect.any(String),
+          ...expenseData
+        }
+      });
+      return database.ref(`expenses/${actions[0].expense.id}`).once("value");
+    })
+    .then(snapshot => {
+      expect(snapshot.val()).toEqual(expenseData);
+      done();
+    });
 });
 
-test("should add expense with defaults to database and store", () => {});
+test("should add expense with defaults to database and store", done => {
+  const store = createMockStore({});
+  const expenseDefaults = {
+    description: "",
+    note: "",
+    amount: 0,
+    createdAt: 0
+  };
+  store
+    .dispatch(startAddExpense(expenseDefaults))
+    .then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual({
+        type: "ADD_EXPENSE",
+        expense: {
+          id: expect.any(String),
+          ...expenseDefaults
+        }
+      });
+      return database.ref(`expenses/${actions[0].expense.id}`).once("value");
+    })
+    .then(snapshot => {
+      expect(snapshot.val()).toEqual(expenseDefaults);
+      done();
+    });
+});
 
 // test('should setup add expense action object with default values', () => {
 //  const expenseData = {
